a8965ee64d4a8fc07fc37735617a1a75e3750c47
fishglitch <fishglitch@users.noreply.github.com>
2025-02-07 00:26:19 -0800

    passed v7_startTransition: true to future prop to activate flag for ReactRouter7

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,10 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter future={{ v7_relativeSplatPath: true }}>
+    <BrowserRouter future={{ 
+      v7_relativeSplatPath: true,
+      v7_startTransition: true,
+      }}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
